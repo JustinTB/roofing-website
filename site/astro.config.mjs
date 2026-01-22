9805479751febba9f1484f6327139bfd0975067d
JustinTB <jtbwx7@gmail.com>
2026-01-22 20:12:02 +0000

    Fix base path config

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://JustinTB.github.io/roofing-website/',
-  base: '/roofing-website',
+  base: '/roofing-website/',
 });
